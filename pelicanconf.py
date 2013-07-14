1d9448252fbb757d109fda6bcd420ed767d8565c
Martin Putniorz <mputniorz@gmail.com>
2013-07-14 20:06:09 +0200

    Ready to serve

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Martin Putniorz'
 SITENAME = u'bysputnikus by Martin Putniorz'
-SITEURL = ''
+SITEURL = 'sputnikus.github.io'
 
 TIMEZONE = 'Europe/Paris'
 
@@ -15,17 +15,14 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 10
+
+THEME = "bysputnikus_theme"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+TEMPLATE_PAGES = {'pages/about.html': 'about.html'}
+
+TWITTER_USERNAME = 'sputnikus'
+FLATTR_USERNAME = 'sputnikus'
